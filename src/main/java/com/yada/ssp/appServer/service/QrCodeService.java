5c2845a8508f03118af3d6dad31ae41240bcb92d
lirenhao <renhao.li@bjyada.com>
2018-09-05 13:10:39 +0800

    备注修改

--- src/main/java/com/yada/ssp/appServer/service/QrCodeService.java
+++ src/main/java/com/yada/ssp/appServer/service/QrCodeService.java
@@ -22,18 +22,18 @@ public class QrCodeService {
     public Map<String, String> getQrCode(String amt, UserInfoPK id) {
         UserInfo userInfo = userInfoService.getUserInfo(id);
         String termLsNo = String.valueOf(new Date().getTime());
+        // 交易类型 动态二维码请求-01、交易渠道 目前默认填01
         return getQrCode("01", amt, termLsNo, userInfo.getCry(), userInfo.getTermNo(), userInfo.getMerNo(), "01");
     }
 
     /**
-     *
-     * @param tranType 交易类型 动态二维码请求-01
-     * @param tranAmt 金额
+     * @param tranType 交易类型
+     * @param tranAmt  金额
      * @param termLsNo 终端流水号
-     * @param tranCry 币种
-     * @param termNo 终端号
-     * @param merNo 商户号
-     * @param channel 交易渠道 目前默认填01
+     * @param tranCry  币种
+     * @param termNo   终端号
+     * @param merNo    商户号
+     * @param channel  交易渠道
      * @return 报文结果
      */
     private Map<String, String> getQrCode(String tranType, String tranAmt, String termLsNo,
@@ -53,21 +53,27 @@ public class QrCodeService {
     public Map<String, String> queryResult(String queryNo, UserInfoPK id) {
         String termNo = userInfoService.getTermNo(id);
         String termLsNo = String.valueOf(new Date().getTime());
+        // 交易类型 动态二维码请求-02
         return queryResult("02", termLsNo, termNo, id.getMerNo(), queryNo);
     }
 
     /**
-     *
-     * @param tranType 交易类型 动态二维码请求-02
+     * @param tranType 交易类型
      * @param termLsNo 终端流水号
-     * @param termNo 终端号
-     * @param merNo 商户号
-     * @param queryNo 交易查询号
+     * @param termNo   终端号
+     * @param merNo    商户号
+     * @param queryNo  交易查询号
      * @return 报文结果
      */
     private Map<String, String> queryResult(String tranType, String termLsNo,
                                             String termNo, String merNo, String queryNo) {
         // TODO 发起交易查询请求
+        Map<String, String> resp = new HashMap<>();
+        resp.put("respCode", "00");
+        resp.put("respMsg", "成功");
+        resp.put("tranNo", "");
+        resp.put("tranAmt", "");
+        resp.put("tranCry", "");
         return null;
     }
 }
